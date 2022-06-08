f572ee20b9904a0eb627e603014658f5a8823df6
matshedis <matshediso@softstartbti.co.za>
2022-06-08 15:58:15 +0200

    new icon for department sidebar

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -15,7 +15,7 @@
                 <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
                     <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Dashboard</a></li>
                     <li class="no-padding">
-                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Department<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apartment</i>Department<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
                         <div class="collapsible-body">
                             <ul>
                                 <li><a href="adddepartment.php">Add Department</a></li>
