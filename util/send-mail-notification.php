b7d95a5ddb654d24a0f034af623fe354baad5070
DipsMethi <dipolelodips@gmail.com>
2022-06-17 12:56:54 +0200

    Email feature

--- util/send-mail-notification.php
+++ util/send-mail-notification.php
@@ -1,5 +1,13 @@
 <?php
 
-    
+    $toEmail = "dipolelodips@gmail.com";
+    $subject = "Leave Application";
+    $body = "Hello someone applied for a leave click here to approve 192.168.27.121/elms/supervisor/";
+    $headers = "From: sender\'s email";
+
+    if(mail($toEmail, $subject, $body, $headers))
+        echo "Email successfully sent to $toEmail...";
+    else
+        echo "Email sending failed...";
 
 ?>
