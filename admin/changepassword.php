92426b18c2737e5666aab35c1123db86a5a715ed
DipsMethi <dipolelodips@gmail.com>
2022-07-03 12:10:56 +0200

    Point to employees table

--- admin/changepassword.php
+++ admin/changepassword.php
@@ -9,28 +9,29 @@ header('location:index.php');
 else{
 // Code for change password 
 if(isset($_POST['change']))
-    {
-$password=md5($_POST['password']);
-$newpassword=md5($_POST['newpassword']);
-$username=$_SESSION['alogin'];
-    $sql ="SELECT Password FROM admin WHERE UserName=:username and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':username', $username, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results = $query -> fetchAll(PDO::FETCH_OBJ);
-if($query -> rowCount() > 0)
 {
-$con="update admin set Password=:newpassword where UserName=:username";
-$chngpwd1 = $dbh->prepare($con);
-$chngpwd1-> bindParam(':username', $username, PDO::PARAM_STR);
-$chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
-$chngpwd1->execute();
-$msg="Your Password succesfully changed";
-}
-else {
-$error="Your current password is wrong";    
-}
+    $password=md5($_POST['password']);
+    $newpassword=md5($_POST['newpassword']);
+    $username=$_SESSION['alogin'];
+
+    $sql ="SELECT Password FROM employees WHERE username=:username and password=:password";
+    $query= $dbh -> prepare($sql);
+    $query-> bindParam(':username', $username, PDO::PARAM_STR);
+    $query-> bindParam(':password', $password, PDO::PARAM_STR);
+    $query-> execute();
+    $results = $query -> fetchAll(PDO::FETCH_OBJ);
+    
+    if($query->rowCount() > 0)
+    {
+        $con="UPDATE admin SET Password=:newpassword where username=:username";
+        $chngpwd1 = $dbh->prepare($con);
+        $chngpwd1-> bindParam(':username', $username, PDO::PARAM_STR);
+        $chngpwd1-> bindParam(':newpassword', $newpassword, PDO::PARAM_STR);
+        $chngpwd1->execute();
+        $msg="Your Password succesfully changed";
+    }
+    else 
+        $error="Your current password is wrong";    
 }
 ?>
 
